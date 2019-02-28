2f8016d30aee4b7aee781368c234f89bf6bb8d0b
Erik <vfxbaliof@gmail.com>
2019-02-28 18:50:19 +0200

    03. Student System

---
+++ WORKING WITH ABSTRACTION/03. Student System/src/StudentRepository.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class StudentRepository {
+    private Map<String, Student> data;
+
+    public StudentRepository() {
+        this.data = new HashMap<>();
+    }
+
+    public boolean existsByName(String name) {
+        return this.data.containsKey(name);
+    }
+
+    public void save(Student student) {
+        this.data.put(student.getName(), student);
+    }
+
+    public Student findByName(String name) {
+        return this.data.get(name);
+    }
+}

4cb35edd3fd472a67e9ded200e8790aa4ae718e3
Erik <vfxbaliof@gmail.com>
2019-03-07 16:46:04 +0200

    01. Class Box

---
+++ EXERCISE ENCAPSULATION/01. Class Box/src/Main.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        double length = Double.parseDouble(bufferedReader.readLine());
+        double width = Double.parseDouble(bufferedReader.readLine());
+        double height = Double.parseDouble(bufferedReader.readLine());
+
+        Box box = new Box(length, width, height);
+
+        System.out.println(box.toString());
+    }
+}

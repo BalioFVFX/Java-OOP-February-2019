7cb9af6d5e5a6f88a2c6484ffe355c14626a79e0
Erik <vfxbaliof@gmail.com>
2019-03-09 16:39:44 +0200

    03. Hierarchical Inheritance

---
+++ INHERITANCE/03. Hierarchical Inheritance/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Dog puppy = new Dog();
+        puppy.eat();
+        puppy.bark();
+
+        Cat cat = new Cat();
+        cat.eat();
+        cat.meow();
+    }
+}

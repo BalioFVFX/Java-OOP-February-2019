56bb036bf2fba407d9b9470fcd9fe6371dddb792
Erik <vfxbaliof@gmail.com>
2019-03-15 18:51:28 +0200

    06. Border Control

---
+++ INTERFACES AND ABSTRACTION/06. Border Control/src/Robot.java
@@ -0,0 +1,26 @@
+public class Robot implements Identifiable {
+    private String id;
+    private String model;
+
+    public Robot(String id, String model) {
+        this.id = id;
+        this.model = model;
+    }
+
+    @Override
+    public String getId() {
+        return this.id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}

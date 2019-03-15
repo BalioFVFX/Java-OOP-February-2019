a396a4e193aa8dc7de8f2e984aea7bc47fe9d695
Erik <vfxbaliof@gmail.com>
2019-03-15 17:54:06 +0200

    05. Say Hello Extended

---
+++ INTERFACES AND ABSTRACTION/05. Say Hello Extended/src/Chinese.java
@@ -0,0 +1,10 @@
+public class Chinese extends BasePerson implements Person {
+    public Chinese(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sayHello() {
+        return "Djydjybydjy";
+    }
+}

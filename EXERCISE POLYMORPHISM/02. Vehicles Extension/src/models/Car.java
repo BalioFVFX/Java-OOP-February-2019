87c02ac28ea1fdca7019af0c6bd9a8af0b69b72e
Erik <vfxbaliof@gmail.com>
2019-03-18 20:12:31 +0200

    02. Vehicles Extension

---
+++ EXERCISE POLYMORPHISM/02. Vehicles Extension/src/models/Car.java
@@ -0,0 +1,10 @@
+package models;
+
+import models.base.Vehicle;
+
+public class Car extends Vehicle implements Drivable {
+
+    public Car(double fuelQuantity, double fuelConsumption, double tankCapacity) {
+        super(fuelQuantity, fuelConsumption, tankCapacity, 0.9);
+    }
+}

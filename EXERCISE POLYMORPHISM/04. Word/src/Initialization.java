5532ad200255f8c6a6cdfde91a1d9fbecc396f11
Erik <vfxbaliof@gmail.com>
2019-03-21 17:37:01 +0200

    04. Word

---
+++ EXERCISE POLYMORPHISM/04. Word/src/Initialization.java
@@ -0,0 +1,8 @@
+public class Initialization{
+
+    public static CommandInterface buildCommandInterface(StringBuilder text){
+        CommandInterface commandInterface = new CommandImpl(text);
+        commandInterface.init();
+        return commandInterface;
+    }
+}

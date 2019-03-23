4fd0fc4aa9f45b0d2e674b22fae95dc8aadbef56
Erik <vfxbaliof@gmail.com>
2019-03-23 20:47:50 +0200

    05. Coding Tracker

---
+++ REFLECTION AND ANNOTATION/05. Coding Tracker/src/Tracker.java
@@ -0,0 +1,16 @@
+public class Tracker {
+    @Author(name = "Pesho")
+    public void test(){
+
+    }
+
+    @Author(name = "Gosho")
+    private static void methodByGosho(){
+
+    }
+
+    @Author(name = "Gosho")
+    public String tester(){
+        return "Test";
+    }
+}
